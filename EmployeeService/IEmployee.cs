57a66039f5ca40afc095c481a7ae1e4de9a7567d
Vamshikrishna <Vamshikrishna.Bose@Ltts.com>
2021-05-07 13:51:17 +0530

    implementing simple dependency injection using AddSingleton method

---
+++ EmployeeService/IEmployee.cs
@@ -0,0 +1,21 @@
+﻿using DependencyInjection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.EmployeeService
+{
+   public interface IEmployee
+    {
+
+        List<Employee> getEmployees();
+
+        Employee getEmployee(int id);
+        Employee AddNewEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
+    }
+}
---
+++ EmployeeService/MockEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Models;
+namespace DependencyInjection.EmployeeService
+{
+    public class MockEmployee : IEmployee
+    {
+        private List<Employee> employees = new List<Employee>() {
+        new Employee(){ id = 1, Name = "First Employee"},
+        new Employee(){ id = 2, Name = "Second Employee"}
+        };
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+       public Employee getEmployee(int id) {
+            throw new NotImplementedException();
+        }
+       public   Employee AddNewEmployee(Employee employee) {
+            throw new NotImplementedException();
+        }
+
+       public   void DeleteEmployee(Employee employee) {
+            throw new NotImplementedException();
+        }
+
+      public  Employee EditEmployee(Employee employee) {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> getEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
